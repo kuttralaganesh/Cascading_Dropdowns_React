17b87dec7d13252eede1ae0c156f01cb289624ef
kuttralaganesh <44425346+kuttralaganesh@users.noreply.github.com>
2023-08-18 13:22:54 +0530

    country app

--- src/App.js
+++ src/App.js
@@ -4,19 +4,51 @@ import { useState, useEffect } from 'react';
 
 export default function App() {
   const countries = [
-    { id: 1, name: 'US' },
-    { id: 2, name: 'INDIA' },
+    { id: 1, name: 'INDIA' },
+    { id: 2, name: 'US' },
+  ];
+
+  const state = [
+    { id: 1, countryId: 1, stateName: 'Tamil Nadu' },
+    { id: 2, countryId: 1, stateName: 'Kerala' },
+    { id: 3, countryId: 1, stateName: 'Bangalore' },
+    { id: 4, countryId: 2, stateName: 'US_1' },
+    { id: 5, countryId: 2, stateName: 'US_2' },
   ];
 
   const [countrie, setCountrie] = useState([]);
+  const [states, setStates] = useState([]);
+
   useEffect(() => {
     setCountrie(countries);
-  },[]);
+  }, []);
+
+  const handleCountry = (id) => {
+    const countrystate = state.filter((value) => {
+      return value.countryId == id;
+    });
+    console.log(countrystate);
+    setStates(countrystate);
+  };
 
   return (
     <>
-      <select>
+      <select onChange={(e) => handleCountry(e.target.value)}>
         <option value={0}>Select Country</option>
+        {countrie.map((item, index) => {
+          return (
+            <option key={item.id} value={item.id}>
+              {item.name}
+            </option>
+          );
+        })}
+      </select>
+
+      <select>
+        <option value={0}>Select-State</option>
+        {states.map((value) => {
+          return <option key={value.id}>{value.stateName}</option>;
+        })}
       </select>
     </>
   );
